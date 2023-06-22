a448d095b0641be409efeb7422cbf47bbbb25f29
Crowdbotics <engineering@crowdbotics.com>
2023-06-22 10:00:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-night-dev-93871",
+  url: "https://gentle-night-dev-93871.botics.co",
+  api: "https://gentle-night-dev-93871.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
